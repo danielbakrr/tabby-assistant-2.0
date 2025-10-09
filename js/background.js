1827a609e5745fe43d89313b3357bf3384934228
glizzygobblrr <S10257120@connect.np.edu.sg>
2025-10-09 17:04:57 +0800

    amend onInstalled listener to prevent constant refreshing

--- js/background.js
+++ js/background.js
@@ -1,11 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
+  //inject content script into all tabs to avoid refreshinh
+  chrome.tabs.query({}, (tabs) => {
+    for (let tab of tabs) {
+      if (tab.url.startsWith("http")) {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ['content.js']
+        });
+      }
+    }
+  });
+
     if (chrome.sidePanel && chrome.sidePanel.setPanelBehavior){
         chrome.sidePanel
         .setPanelBehavior({ openPanelOnActionClick: true })
         .catch((error) => console.error(error));
     }
-}); 
+});
 
+
+//listen for messages from content script
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.type === "selected") {
     chrome.runtime.sendMessage({ type: "selection_for_panel", text: message.text, tab: sender.tab });
