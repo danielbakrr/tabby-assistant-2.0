df931350132818b1a363e9f3e328efcf764c660b
glizzygobblrr <S10257120@connect.np.edu.sg>
2025-10-09 14:56:06 +0800

    remove surroundContents to prevent  dupe logic

--- content.js
+++ content.js
@@ -4,21 +4,24 @@ document.addEventListener('selectionchange', function() {
     if (!selected) return;
 
     const text = selected ? selected.toString().trim() : '';
-    if (selected) {
+    if (!selected) return;
+    
+    try{
         const range = window.getSelection().getRangeAt(0);
         const highlightSpan = document.createElement('span');
         highlightSpan.style.backgroundColor = 'yellow';
-        range.surroundContents(highlightSpan); // easier to see
         highlightSpan.dataset.tempHighlight = 'true'; // mark for removal later
 
-        const contents = range.extractContents();
+        const contents = range.extractContents(); 
         highlightSpan.appendChild(contents);
         range.insertNode(highlightSpan);
 
         selected.removeAllRanges();
         chrome.runtime.sendMessage({ type: 'selection', text});
-
-}});
+    } catch (e) {
+        console.error('Error highlighting selection:', e);
+    }
+});
 
 
 //remove highlighted selection upon click
