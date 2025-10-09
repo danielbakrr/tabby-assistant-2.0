b1c0893d264ff7ea2f91359c08a07862112826b2
glizzygobblrr <S10257120@connect.np.edu.sg>
2025-10-09 15:12:05 +0800

    remove yellow highlight feature

--- content.js
+++ content.js
@@ -7,26 +7,8 @@ document.addEventListener('selectionchange', function() {
     if (!text) return;
     
     try{
-        const range = window.getSelection().getRangeAt(0);
-        const highlightSpan = document.createElement('span');
-        highlightSpan.style.backgroundColor = 'yellow';
-        highlightSpan.dataset.tempHighlight = 'true'; // mark for removal later
-
-        const contents = range.extractContents(); 
-        highlightSpan.appendChild(contents);
-        range.insertNode(highlightSpan);
-
-        selected.removeAllRanges();
         chrome.runtime.sendMessage({ type: 'selected', text});
     } catch (e) {
         console.error('Error highlighting selection:', e);
     }
-});
-
-
-//remove highlighted selection upon click
-document.addEventListener("click", () => {
-  document.querySelectorAll("span[data-temp-highlight]").forEach(span => {
-    span.replaceWith(...span.childNodes);
-  });
 });
