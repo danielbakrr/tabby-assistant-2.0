47e65657f35bde4ab1db5237c2cab3f7e06b2657
glizzygobblrr <S10257120@connect.np.edu.sg>
2025-10-21 17:31:26 +0800

    adjust background script to handle chrome runtime listeners

--- js/background.js
+++ js/background.js
@@ -18,3 +18,29 @@ chrome.runtime.onMessage.addListener((message, sender) => {
     }
     }
 });
+
+//timer automation handling
+let startTime = null;
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    try{
+        if (msg.action === "start" && !startTime) {
+            startTime = Date.now();
+            console.log("Timer started:", new Date(startTime).toLocaleTimeString());
+        }
+
+        if (msg.action === "stop" && startTime) {
+            const elapsed = Math.floor((Date.now() - startTime) / 60000); //minutes
+            startTime = null;
+
+            chrome.storage.local.get("totalTime", (data) => {
+            const updatedTotal = (data.totalTime || 0) + elapsed;
+            chrome.storage.local.set({ totalTime: updatedTotal });
+            console.log(`Timer stopped. Session: ${elapsed} min. Total: ${updatedTotal} min.`);
+            });
+        }
+    } catch(e){
+        console.log("Error starting timer:", e)
+    }
+  }
+);
--- js/timer.js
+++ js/timer.js
@@ -1,24 +1,24 @@
-let startTime = null;
+// let startTime = null;
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    try{
-        if (msg.action === "start" && !startTime) {
-            startTime = Date.now();
-            console.log("Timer started:", new Date(startTime).toLocaleTimeString());
-        }
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//     try{
+//         if (msg.action === "start" && !startTime) {
+//             startTime = Date.now();
+//             console.log("Timer started:", new Date(startTime).toLocaleTimeString());
+//         }
 
-        if (msg.action === "stop" && startTime) {
-            const elapsed = Math.floor((Date.now() - startTime) / 60000); //minutes
-            startTime = null;
+//         if (msg.action === "stop" && startTime) {
+//             const elapsed = Math.floor((Date.now() - startTime) / 60000); //minutes
+//             startTime = null;
 
-            chrome.storage.local.get("totalTime", (data) => {
-            const updatedTotal = (data.totalTime || 0) + elapsed;
-            chrome.storage.local.set({ totalTime: updatedTotal });
-            console.log(`Timer stopped. Session: ${elapsed} min. Total: ${updatedTotal} min.`);
-            });
-        }
-    } catch(e){
-        console.log("Error starting timer:", e)
-    }
-  }
-);
+//             chrome.storage.local.get("totalTime", (data) => {
+//             const updatedTotal = (data.totalTime || 0) + elapsed;
+//             chrome.storage.local.set({ totalTime: updatedTotal });
+//             console.log(`Timer stopped. Session: ${elapsed} min. Total: ${updatedTotal} min.`);
+//             });
+//         }
+//     } catch(e){
+//         console.log("Error starting timer:", e)
+//     }
+//   }
+// );
